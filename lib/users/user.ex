54ed261060b4f29851bd69b50e31d8dd75f6be6b
freddneos <fredd@neosdev.com.br>
2021-08-27 18:44:20 +0100

    user context

---
+++ lib/users/user.ex
@@ -0,0 +1,18 @@
+defmodule Elixvery.Users.User do
+  @keys [:name, :email, :document, :age]
+  @enforce_keys @keys
+
+  defstruct @keys
+
+  def build(name, email, document, age) when age >= 18 and is_bitstring(document) do
+    {:ok,
+     %__MODULE__{
+       name: name,
+       email: email,
+       document: document,
+       age: age
+     }}
+  end
+
+  def build(_name, _email, _document, _age), do: {:error, "Invalid Parameters"}
+end
